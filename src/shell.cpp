792b9f29d001565fd643d06574e22125e5cebd89
yehudaheller <116564740+yehudaheller@users.noreply.github.com>
2023-06-12 15:35:21 +0300

    part 2 done

--- src/shell.cpp
+++ src/shell.cpp
@@ -1,43 +1,118 @@
 #include "shell.h"
+#include <fcntl.h>  // for file descriptor manipulation
 
 Shell::Shell() {}
 
 void Shell::run() {
+   
     std::string input;
+    std::vector<BackgroundProcess> backgroundProcesses;
 
-    while (true) {
+    while (true)
+    {
         std::cout << "Shell> ";
         std::getline(std::cin, input);
 
-        if (input == "exit") {
+        if (input == "exit")
             break;
-        }
 
-        if (input == "myjobs") {
-            updateBackgroundProcesses();
+        if (input == "myjobs")
+        {
             showBackgroundProcesses();
             continue;
         }
 
-        // Check if the command should be run in the background
-        bool runInBackground = false;
-        if (!input.empty() && input.back() == '&') {
-            runInBackground = true;
-            input.pop_back();
+        std::vector<PipeCommand> pipeCommands;
+
+        // Split the input by pipe symbol '|'
+        std::vector<std::string> commands = splitString(input, '|');
+        for (const auto& cmd : commands) {
+            std::string trimmedCmd = cmd;
+            if (!trimmedCmd.empty() && trimmedCmd.front() == ' ')
+                trimmedCmd = trimmedCmd.substr(1);
+            if (!trimmedCmd.empty() && trimmedCmd.back() == ' ')
+                trimmedCmd.pop_back();
+            bool runInBackground = false;
+            if (!trimmedCmd.empty() && trimmedCmd.back() == '&') {
+                runInBackground = true;
+                trimmedCmd.pop_back();
+            }
+            pipeCommands.push_back({ trimmedCmd, runInBackground });
         }
 
-        executeCommand(input, runInBackground);
+        executePipeline(pipeCommands, backgroundProcesses);
     }
+
 }
 
 void Shell::executeCommand(const std::string& command, bool runInBackground) {
+    std::vector<std::string> args = splitString(command, ' ');
+
+    // Check if the command should be run in the background
+    if (!args.empty() && args.back() == "&") {
+        runInBackground = true;
+        args.pop_back();
+    }
+
+    // Check if input/output redirection is requested
+    std::string inputFile;
+    std::string outputFile;
+    bool inputRedirection = false;
+    bool outputRedirection = false;
+    size_t inputIndex = 0;
+    size_t outputIndex = 0;
+
+    for (size_t i = 0; i < args.size(); i++) {
+        if (args[i] == "<") {
+            if (i + 1 < args.size()) {
+                inputRedirection = true;
+                inputFile = args[i + 1];
+                inputIndex = i;
+            }
+        } else if (args[i] == ">") {
+            if (i + 1 < args.size()) {
+                outputRedirection = true;
+                outputFile = args[i + 1];
+                outputIndex = i;
+            }
+        }
+    }
+
+    // Remove the redirection symbols and file names from the arguments
+    if (inputRedirection) {
+        args.erase(args.begin() + inputIndex, args.begin() + inputIndex + 2);
+    }
+    if (outputRedirection) {
+        args.erase(args.begin() + outputIndex, args.begin() + outputIndex + 2);
+    }
+
     pid_t pid = fork();
     if (pid == -1) {
         std::cerr << "Failed to create child process" << std::endl;
         return;
     } else if (pid == 0) {
         // Child process
-        std::vector<std::string> args = splitString(command, ' ');
+        // Handle input redirection
+        if (inputRedirection) {
+            int inputFileDescriptor = open(inputFile.c_str(), O_RDONLY);
+            if (inputFileDescriptor == -1) {
+                std::cerr << "Failed to open input file: " << inputFile << std::endl;
+                exit(EXIT_FAILURE);
+            }
+            dup2(inputFileDescriptor, STDIN_FILENO);
+            close(inputFileDescriptor);
+        }
+
+        // Handle output redirection
+        if (outputRedirection) {
+            int outputFileDescriptor = open(outputFile.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (outputFileDescriptor == -1) {
+                std::cerr << "Failed to open output file: " << outputFile << std::endl;
+                exit(EXIT_FAILURE);
+            }
+            dup2(outputFileDescriptor, STDOUT_FILENO);
+            close(outputFileDescriptor);
+        }
 
         // Convert vector of strings to char* array
         std::vector<char*> cArgs;
@@ -105,3 +180,57 @@ void Shell::updateBackgroundProcesses() {
         }
     }
 }
+
+
+void Shell::executePipeline(const std::vector<PipeCommand>& commands, std::vector<BackgroundProcess>& backgroundProcesses) {
+    std::vector<int> pipes(commands.size() - 1);
+
+    for (size_t i = 0; i < commands.size(); ++i) {
+        int pipeFD[2];
+        if (i < commands.size() - 1) {
+            if (pipe(pipeFD) == -1) {
+                std::cerr << "Failed to create pipe" << std::endl;
+                return;
+            }
+            pipes[i] = pipeFD[0];
+        }
+
+        pid_t pid = fork();
+
+        if (pid == -1) {
+            std::cerr << "Failed to create child process" << std::endl;
+            return;
+        } else if (pid == 0) {
+            // The Child process:
+            if (i > 0) {
+                dup2(pipes[i - 1], STDIN_FILENO);
+                close(pipes[i - 1]);
+            }
+
+            if (i < commands.size() - 1) {
+                dup2(pipeFD[1], STDOUT_FILENO);
+                close(pipeFD[0]);
+                close(pipeFD[1]);
+            }
+
+            executeCommand(commands[i].command, commands[i].runInBackground);
+
+            exit(EXIT_SUCCESS);
+        } else {
+            if (i > 0)
+                close(pipes[i - 1]);
+
+            if (!commands[i].runInBackground) {
+                int status;
+                waitpid(pid, &status, 0);
+                if (status != 0)
+                    std::cerr << "Command exited with non-zero status: " << commands[i].command << std::endl;
+            }
+        }
+    }
+
+    for (int pipeFD : pipes)
+        close(pipeFD);
+}
+
+
