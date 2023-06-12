acf94782ddb38adcbded62a4c0ecdf014bec132a
yehudaheller <116564740+yehudaheller@users.noreply.github.com>
2023-06-12 15:35:51 +0300

    part 2 done

--- include/shell.h
+++ include/shell.h
@@ -18,6 +18,11 @@ private:
         pid_t pid;             // Process ID
         std::string command;   // Command string
     };
+    
+    struct PipeCommand {
+		std::string command;
+		bool runInBackground;
+	};
 
     void executeCommand(const std::string& command, bool runInBackground);
     std::vector<std::string> splitString(const std::string& input, char delimiter);
@@ -25,6 +30,16 @@ private:
     void updateBackgroundProcesses();
 
     std::vector<BackgroundProcess> backgroundProcesses;
+
+    // New helper function to execute piped commands
+    void executePipedCommands(const std::string& command);
+
+    // New helper function to handle writing to the pipe
+    void writeToPipe(int pipefd[], int inputFd);
+
+    // New helper function to handle reading from the pipe
+    void readFromPipe(int pipefd[], int outputFd);
+    
+    void executePipeline(const std::vector<PipeCommand>& commands, std::vector<BackgroundProcess>& backgroundProcesses) ;
+
 };
-
-
